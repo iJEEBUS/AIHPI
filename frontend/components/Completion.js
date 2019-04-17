e912ebc544c007470e8d2928b785ef490d4fc5a5
Eric Schoenborn <cricketevs@gmail.com>
2019-04-16 17:55:42 -0700

    end of game animations

--- frontend/components/Completion.js
+++ frontend/components/Completion.js
@@ -7,9 +7,46 @@ class Completion extends Component {
     constructor(props) {
         super(props);
         this.state = {
+            currentScore:0,
+            score:50,
+            stars:[],
+            commentNumber: 0
         };
     }
-    render() {
+    scoreChange = () =>{
+        if(this.state.currentScore <= this.state.score){
+            var currentScore
+            var commentNumber = 0;
+            if(this.state.currentScore < this.state.score){
+                currentScore = this.state.currentScore +1;
+            }else{
+                currentScore = this.state.score;
+                if(this.state.score > 80){
+                    commentNumber = 3;
+                }else if(this.state.score > 50){
+                    commentNumber = 2;
+                }else{
+                    commentNumber = 1;
+                }
+            }
+            var starScore =20;
+            var stars = []
+            while(starScore <= this.state.currentScore){
+               stars.push(faStar)
+               starScore =starScore+ 20; 
+            }
+            if(starScore -10 <= this.state.currentScore){
+                stars.push(faStarHalf)
+            }
+            this.setState({ 
+                currentScore:  currentScore,
+                stars: stars,
+                commentNumber: commentNumber
+            })
+        }
+    }
+    componentDidMount() {
+        this.interval = setInterval(() => this.scoreChange(), 50);
         var meh =0;
         const worth = 1/ this.props.rounds
         this.props.data.filter(function(value) {
@@ -18,15 +55,10 @@ class Completion extends Component {
             }
           });
         const score = (meh.toFixed(3)) *100;
-        var starScore =20;
-        var stars = []
-        while(starScore <= score){
-           stars.push(faStar)
-           starScore =starScore+ 20; 
-        }
-        if(starScore -10 <= score){
-            stars.push(faStarHalf)
-        }
+        this.setState({ score:  score})
+    }
+    render() {
+        const comments = [""," Better Luck Next Time", "Nice!", "Great Job!"];
     return (
         <div>
             <div className="modal-wrapper"
@@ -35,15 +67,15 @@ class Completion extends Component {
                     opacity: this.props.show ? '1' : '0'
                 }}>
                 <div className="modal-header">
-                    <h3>Congradulations!</h3>
+                    <h3>Congratulations!</h3>
                     <span className="close-modal-btn" onClick={this.props.close}>×</span>
                 </div>
                 <div className="modal-body">
                     <p>
-                    You have completed the game with a score of: {score}%!
-                    Great Job!
+                    You have completed the game with a score of: {this.state.currentScore}%!<br/>
+                    {comments[this.state.commentNumber]}
                     </p>
-                    {stars.map(i => (         
+                    {this.state.stars.map(i => (         
                         <FontAwesomeIcon icon={i} size={"2x"} />      
                     ))}
 
